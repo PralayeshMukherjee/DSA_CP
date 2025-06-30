41e45ea780aaa835d7378de9281c79ec92fc1f9a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2025-06-30 18:17:56 +0530

    add the solution of the question the length of the longest substring

--- DailyDSA/Sliding_Window&Two_Pointer_Combined_Problems/Medium_Problems/q1.java
+++ DailyDSA/Sliding_Window&Two_Pointer_Combined_Problems/Medium_Problems/q1.java
@@ -1,6 +1,27 @@
+
 // Longest Substring Without Repeating Characters
 // link:- https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+import java.util.HashMap;
+import java.util.Map;
 
 public class q1 {
-
+    public int lengthOfLongestSubstring(String s) {
+        int i = 0, j = 0, count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        while (i < s.length() && j < s.length()) {
+            char ch = s.charAt(j);
+            if (!map.containsKey(ch) || map.get(ch) < i) {
+                count = Math.max(count, j - i + 1);
+                map.put(ch, j);
+            } else {
+                i = map.get(ch) + 1;
+                map.remove(ch);
+                map.put(ch, j);
+            }
+            j++;
+        }
+        return count;
+    }
 }
+// TC:- O(2N)(some times i and j can traverse whole array)
+// SC:- O(N)
